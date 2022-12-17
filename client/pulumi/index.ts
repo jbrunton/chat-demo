33c7fab205ad97706ca7ef1a4d10ca496a65c9ec
John Brunton <john_brunton@live.co.uk>
2022-12-17 19:34:23 +0000

    feat: deploy client to AWS

--- client/pulumi/index.ts
+++ client/pulumi/index.ts
@@ -1,20 +1,9 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws-native";
+import * as staticwebsite from "@pulumi/aws-static-website";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const site = new staticwebsite.Website("client", {
+  sitePath: "../app/dist",
+  targetDomain: "auth0-test.jbrunton-aws.com",
+  withCDN: true,
+});
 
-let siteDir = "www"; // directory for content files
-
-// For each file in the directory, create an S3 object stored in `siteBucket`
-for (let item of require("fs").readdirSync(siteDir)) {
-    let filePath = require("path").join(siteDir, item);
-    let object = new aws.s3.BucketObject(item, {
-      bucket: siteBucket,
-      source: new pulumi.asset.FileAsset(filePath),     // use FileAsset to point to a file
-      contentType: mime.getType(filePath) || undefined, // set the MIME type of the file
-    });
-}
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
+export const websiteURL = site.websiteURL;
