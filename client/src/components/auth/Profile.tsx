ec0dbfad2210e367537be58d0819cae9c0964eb5
John Brunton <john_brunton@live.co.uk>
2022-12-20 18:36:36 +0000

    feat: add sign in with Auth0

---
+++ client/src/components/auth/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useAuth0, User } from "@auth0/auth0-react";
+
+export const Profile: React.FC<{ user: User }> = ({ user }) => {
+  return (
+    <div>
+      <img src={user.picture} alt={user.name} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+    </div>
+  );
+};
