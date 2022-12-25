33a6961a456537752fcb1bcefee0a86ebf45cd45
John Brunton <john_brunton@live.co.uk>
2022-12-25 21:34:34 +0000

    fix: client errors and logging

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,12 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
+const logger = new Logger("main");
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  console.log(`Running server on PID=${process.pid}, port=${port}`);
+  logger.log(`Running server on PID=${process.pid}, port=${port}`);
   await app.listen(port);
 }
 bootstrap();
