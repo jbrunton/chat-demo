d9e12fae4842a189c6a829532e6d8ea91d1999cc
John Brunton <john_brunton@live.co.uk>
2022-12-29 20:13:34 +0000

    feat: add authorization

--- services/api/src/messages/messages.controller.ts
+++ services/api/src/messages/messages.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { Auth } from '../auth/auth.decorator';
 
 @Controller('messages')
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
+  @Auth()
   @Post()
   createMessage(@Body() createMessageDto: CreateMessageDto) {
     return this.messagesService.create(createMessageDto);
   }
 
+  @Auth()
   @Get('/:roomId')
   getMessages(@Param('roomId') roomId: string) {
     return this.messagesService.findForRoom(roomId);
