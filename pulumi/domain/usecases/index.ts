eba8693f0f520f46a25efc58bb24382cda0210be
John Brunton <john_brunton@live.co.uk>
2023-01-02 21:18:51 +0000

    ci: use cleanup-pulumi-stacks action

--- pulumi/domain/usecases/index.ts
+++ pulumi/domain/usecases/index.ts
@@ -1,4 +1,2 @@
 export * from "./application/get-application-config";
 export * from "./domain/get-domain-config";
-export * from "./cleanup/get-legacy-stacks";
-export * from "./cleanup/cleanup-legacy-stacks";
