dae440ffbb05749c48004ca6e04c7a204f07fdc9
John Brunton <john_brunton@live.co.uk>
2023-01-29 14:56:08 +0000

    feat: style scrollbar

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { ChakraProvider } from '@chakra-ui/react'
 import 'highlight.js/styles/default.css'
+import './main.css'
 import App from './App'
 
 const queryClient = new QueryClient()
