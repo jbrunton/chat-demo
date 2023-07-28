d1dbd60d4e30333b08d248f6cf7a6ad3fe0e10b7
John Brunton <john_brunton@live.co.uk>
2023-07-28 14:32:04 +0100

    test: add client tests

---
+++ client/src/features/room/pages/Room.spec.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { screen, waitFor } from '@testing-library/react'
+import { RoomPage } from './Room'
+import { render } from '../../../test/fixtures'
+
+describe('RoomPage', () => {
+  it('loads messages for the room', async () => {
+    render(<RoomPage />, {
+      path: '/room/:roomId',
+      initialEntry: '/room/room:123-can-manage',
+    })
+
+    await waitFor(() => {
+      expect(screen.getByText('Hello, World!'))
+    })
+  })
+
+  it('shows a help message if the user can join the room', async () => {
+    render(<RoomPage />, {
+      path: '/room/:roomId',
+      initialEntry: '/room/room:456-can-join',
+    })
+
+    await waitFor(() => {
+      expect(screen.getByText('You do not have permissions to view messages in this room'))
+    })
+  })
+})
