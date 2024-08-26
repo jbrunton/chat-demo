53ce64d23fb2a957d163522aca7891d4fd3961a7
John Brunton <john_brunton@live.co.uk>
2024-08-26 09:12:10 +0100

    chore: exclude fixtures from mutation tests

--- services/api/stryker.conf.mjs
+++ services/api/stryker.conf.mjs
@@ -3,10 +3,11 @@
 const config = {
   _comment:
     "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
-  packageManager: 'npm',
+  packageManager: 'pnpm',
   reporters: ['html', 'dashboard'],
   testRunner: 'jest',
   plugins: ['@stryker-mutator/jest-runner'],
+  ignorePatterns: ['src/fixtures/**'],
   testRunner_comment:
     'Take a look at https://stryker-mutator.io/docs/stryker-js/jest-runner for information about the jest plugin.',
   coverageAnalysis: 'perTest',
