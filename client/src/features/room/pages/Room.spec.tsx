bfb1e7eb0e07bb3b40e44c9460d42b9943482dea
John Brunton <john_brunton@live.co.uk>
2024-08-31 16:26:09 +0100

    test: add tests for room alerts

--- client/src/features/room/pages/Room.spec.tsx
+++ client/src/features/room/pages/Room.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { screen, waitFor } from '@testing-library/react'
+import { screen, waitFor, within } from '@testing-library/react'
 import { RoomPage } from './Room'
 import { render } from '../../../test/fixtures'
 
@@ -15,7 +15,7 @@ describe('RoomPage', () => {
     })
   })
 
-  it('shows a help message if the user can join the room', async () => {
+  it('shows a message if the user lacks permissions to read the room', async () => {
     render(<RoomPage />, {
       path: '/room/:roomId',
       initialEntry: '/room/room:200-can-join',
@@ -23,6 +23,45 @@ describe('RoomPage', () => {
 
     await waitFor(() => {
       expect(screen.getByText('You do not have permissions to view messages in this room'))
+    })
+  })
+
+  it('shows an alert if the user can join the room', async () => {
+    render(<RoomPage />, {
+      path: '/room/:roomId',
+      initialEntry: '/room/room:200-can-join',
+    })
+
+    await waitFor(() => {
+      const alert = screen.getByRole('alert')
+      expect(alert).toHaveTextContent('Join this room to chat.')
+      expect(within(alert).getByRole('button')).toHaveTextContent('Join')
+    })
+  })
+
+  it('shows an alert if the user can request to join', async () => {
+    render(<RoomPage />, {
+      path: '/room/:roomId',
+      initialEntry: '/room/room:300-can-request-approval',
+    })
+
+    await waitFor(() => {
+      const alert = screen.getByRole('alert')
+      expect(alert).toHaveTextContent('Join this room to chat.')
+      expect(within(alert).getByRole('button')).toHaveTextContent('Request to Join')
+    })
+  })
+
+  it('shows an alert if the user requires an invite to join', async () => {
+    render(<RoomPage />, {
+      path: '/room/:roomId',
+      initialEntry: '/room/room:400-requires-invite',
+    })
+
+    await waitFor(() => {
+      const alert = screen.getByRole('alert')
+      expect(alert).toHaveTextContent('You need an invite to join.')
+      expect(within(alert).queryByRole('button')).not.toBeInTheDocument()
     })
   })
 })
