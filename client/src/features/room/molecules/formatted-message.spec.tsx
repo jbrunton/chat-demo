ca09bb0fe1e89596e8f1ea428fe98da8250f05af
John Brunton <john_brunton@live.co.uk>
2024-08-31 14:26:16 +0100

    test: message formatting

---
+++ client/src/features/room/molecules/formatted-message.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { FormattedMessage } from './FormattedMessage'
+import '@testing-library/jest-dom'
+
+describe('FormattedMessage', () => {
+  it('renders the content', () => {
+    render(<FormattedMessage content='Hello, World!' />)
+    expect(screen.getByText('Hello, World!'))
+  })
+
+  it('renders content as markdown', () => {
+    const content = '# Action Items\n* Item 1\n* Item 2'
+
+    render(<FormattedMessage content={content} />)
+
+    expect(screen.getByRole('heading')).toHaveTextContent('Action Items')
+    expect(screen.getAllByRole('list')).toMatchInlineSnapshot(`
+      [
+        <ul>
+          
+
+          <li>
+            Item 1
+          </li>
+          
+
+          <li>
+            Item 2
+          </li>
+          
+
+        </ul>,
+      ]
+    `)
+  })
+})
