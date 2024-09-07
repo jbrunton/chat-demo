e50f2e774c9c4a224136722812270e9433829d62
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-09-07 23:55:05 +0100

    fix: tests

--- pulumi/app/aws/get-app-spec.spec.ts
+++ pulumi/app/aws/get-app-spec.spec.ts
@@ -45,6 +45,13 @@ describe("getTaskDefinitionSpec", () => {
               protocol: "tcp",
             },
           ],
+          healthCheck: {
+            retries: 3,
+            command: ["CMD-SHELL", "curl -f http://localhost:8080/ || exit 1"],
+            timeout: 5,
+            interval: 30,
+            startPeriod: 5,
+          },
           environment: [
             {
               name: "PORT",
