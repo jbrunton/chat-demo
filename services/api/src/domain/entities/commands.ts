e09d9f141a20118a17be8115c8c5757ba8f8c1b5
John Brunton <1276413+jbrunton@users.noreply.github.com>
2024-09-22 11:21:48 -0400

    docs: add docs for IncomingCommand

--- services/api/src/domain/entities/commands.ts
+++ services/api/src/domain/entities/commands.ts
@@ -1,5 +1,10 @@
 import { IncomingMessage } from '@entities/messages/message';
 
+/**
+ * An incoming message which is a command. Any message with content prefixed by a forward slash is
+ * considered a command, where or not it can be parsed. If it cannot be parsed, it is an invalid
+ * command.
+ */
 export type IncomingCommand = IncomingMessage & {
   content: `/${string}`;
 };
